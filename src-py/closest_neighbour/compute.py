8938552bbd95cc7299433cbd81d08539bfa96e4a
Ruichen Zheng <ankbzpx@hotmail.com>
2023-02-17 16:43:02 +0800

    better variable naming

--- src-py/closest_neighbour/compute.py
+++ src-py/closest_neighbour/compute.py
@@ -9,12 +9,12 @@ def compute(ref_pts, query_pts):
         ref_pts: n x d
         query_pts: m x d
     Returns:
-        ref_closest_dist: distance between ref_pts to their closest neighbour in query_pts (n)
+        ref_to_query_closest_dist: distance from ref_pts to their closest neighbour in query_pts (n)
         ref_closest_index: ref_pts' closest query index (n)
-        query_closest_dist: distance between query_pts to their closest neighbour in ref_pts (m)
-        query_closest_index: query_pts' closest ref index (m)
+        query_to_ref_closest_dist: distance from query_pts to their closest neighbour in ref_pts (m)
+        closest_ref_index: query_pts' closest ref index (m)
     """
-    ref_closest_dist, ref_closest_index, query_closest_dist, query_closest_index = closest_neighbour_bind.compute(
+    ref_to_query_closest_dist, closest_query_index, query_to_ref_closest_dist, closest_ref_index = closest_neighbour_bind.compute(
         np.asfortranarray(ref_pts), np.asfortranarray(query_pts))
 
-    return ref_closest_dist.astype(np.float64), ref_closest_index.astype(np.int64), query_closest_dist.astype(np.float64), query_closest_index.astype(np.int64)
+    return ref_to_query_closest_dist.astype(np.float64), closest_query_index.astype(np.int64), query_to_ref_closest_dist.astype(np.float64), closest_ref_index.astype(np.int64)
