f0174fc977d4e5781248a60211d35c56e1f0ad0b
Ruichen Zheng <ankbzpx@hotmail.com>
2023-02-17 16:38:37 +0800

    update comment

--- src-py/closest_neighbour/compute.py
+++ src-py/closest_neighbour/compute.py
@@ -9,10 +9,10 @@ def compute(ref_pts, query_pts):
         ref_pts: n x d
         query_pts: m x d
     Returns:
-        ref_closest_dist: n
-        ref_closest_index: n
-        query_closest_dist: m
-        query_closest_index: m
+        ref_closest_dist: distance between ref_pts to their closest neighbour in query_pts (n)
+        ref_closest_index: ref_pts' closest query index (n)
+        query_closest_dist: distance between query_pts to their closest neighbour in ref_pts (m)
+        query_closest_index: query_pts' closest ref index (m)
     """
     ref_closest_dist, ref_closest_index, query_closest_dist, query_closest_index = closest_neighbour_bind.compute(
         np.asfortranarray(ref_pts), np.asfortranarray(query_pts))
